750bd161524f7ea1e02af93e3e2d8997c054324d
Dinh Truong Quang <dinhtq@nal.vn>
2022-10-27 20:34:47 +0700

    Hotfix url

--- joblink/companies/urls.py
+++ joblink/companies/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('companies', views.CompanyListView.as_view()),
-    path('companies/<int:id>', views.CompanyDetail.as_view()),
-   
+    path("companies", views.CompanyListView.as_view()),
+    # path('companies/<int:id>', views.CompanyDetail.as_view()),
 ]
