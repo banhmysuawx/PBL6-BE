df9080cae79beab7b6a1f6ac0e1014fefe22fc44
Tri An Nguyen <triandn43@gmail.com>
2022-10-29 01:55:06 +0700

    Review and Rating Company Function v2

--- joblink/companies/views.py
+++ joblink/companies/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.permissions import AllowAny, IsAuthenticated 
 from rest_framework import status
 from companies.models import Company
 from companies.serializers import CompanySerializer
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
