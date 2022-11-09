46234e0cb565ad4ac77670a7f467a49280861d73
Dinh Truong Quang <dinhtq@nal.vn>
2022-11-09 13:58:49 +0700

    Fix Apply Job

--- joblink/applications/serializers.py
+++ joblink/applications/serializers.py
@@ -1,24 +1,41 @@
-from rest_framework import serializers
-
 from accounts.models import User
 from accounts.serializers import UserSerializer
+from applications.models import Application
 from job.models import Job
 from job.serializers import JobSerializer
-from applications.models import Application
+from rest_framework import serializers
+
+
 class ApplyUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = (
-            'id'
-        )
+        fields = "id"
+
 
 class ApplyJobSerializer(serializers.ModelSerializer):
     # user = serializers.SerializerMethodField()
+    id = serializers.IntegerField(read_only=True, required=False)
+    cv = serializers.FileField(required=False, allow_null=True, default=None)
+
     class Meta:
         model = Application
-        fields = '__all__'
-    def get_user(self, obj):
-        return obj.user.id
+        fields = "__all__"
+
+    def create(self, validated_data):
+        data = {
+            "user": validated_data["user"],
+            "job": validated_data["job"],
+            "fullname": validated_data["fullname"],
+            "email": validated_data["user"].email,
+            "cv": validated_data["cv"],
+            "cover_letter": validated_data["cover_letter"],
+            "status": validated_data["status"],
+            "test_result": validated_data["test_result"],
+        }
+        id_application = Application.objects.create(**data)
+        validated_data["id"] = id_application.id
+        return validated_data
+
 
 class ApplicantSerializer(serializers.ModelSerializer):
     # user = ApplyUserSerializer(read_only = True , many =False)
@@ -28,7 +45,18 @@ class ApplicantSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Application
-        fields = ("id","user","job_id", "job", "status", "fullname", "email","cv","cover_letter","applied_user")
+        fields = (
+            "id",
+            "user",
+            "job_id",
+            "job",
+            "status",
+            "fullname",
+            "email",
+            "cv",
+            "cover_letter",
+            "applied_user",
+        )
         # fields = '__all__'
 
     def get_status(self, obj):
@@ -39,6 +67,7 @@ class ApplicantSerializer(serializers.ModelSerializer):
 
     def get_applied_user(self, obj):
         return ApplyJobSerializer(obj.user).data
+
 
 class AppliedJobSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
--- joblink/applications/views.py
+++ joblink/applications/views.py
@@ -1,29 +1,41 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from applications.models import Application
+from applications.serializers import (
+    ApplicantSerializer,
+    AppliedJobSerializer,
+    ApplyJobSerializer,
+)
+from job.models import Job
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import CreateAPIView, ListAPIView
-from rest_framework.permissions import IsAuthenticated , IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+)
 from rest_framework.response import Response
-from applications.serializers import ApplyJobSerializer , ApplicantSerializer,AppliedJobSerializer
-from applications.models import Application
-from job.models import Job
+
+# Create your views here.
+from django.shortcuts import render
+
 
 class ApplyJobApiView(CreateAPIView):
     serializer_class = ApplyJobSerializer
-    # permission_classes = [IsAuthenticatedOrReadOnly,]
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    # permission_classes = [
+    #     IsAuthenticatedOrReadOnly,
+    # ]
 
     def post(self, request, *args, **kwargs):
+        request.data["job"] = kwargs["job_id"]
+        if request.user.is_authenticated:
+            request.data["user"] = request.user.id
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        instance_id = serializer.save()["id"]
+        serializer = ApplyJobSerializer(Application.objects.get(id=instance_id))
+        # Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
     # queryset = Application.objects.all()
 
 
@@ -33,7 +45,7 @@ class AppliedJobsAPIView(ListAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        print("user",user)
+        print("user", user)
         applied_jobs_id = list(Application.objects.filter(user=user.id))
         # print("list",applied_jobs_id)
         return Job.objects.filter(id__in=applied_jobs_id)
