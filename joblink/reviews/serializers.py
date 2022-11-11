9e93c40aea2d669f77a86dc496a7bdff819f7969
Tri An Nguyen <triandn43@gmail.com>
2022-11-11 19:56:05 +0700

    edit reviews

--- joblink/reviews/serializers.py
+++ joblink/reviews/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
 from .models import Review
+# from companies.serializers import CompanySerializer
+from companies.models import Company
+from accounts.models import User
+# from accounts.serializers import UserSerializer
+
+
+class CompanyReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ['id','company_name','company_location','image']
+
+class UserReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','username','email']
 
 class ReviewsSerializer(serializers.ModelSerializer):
     author = serializers.CharField(source='user.username', read_only=True)
+    user = UserReviewSerializer(many = False)
+    company = CompanyReviewSerializer(many = False)
     def get_author(self, object):
         return object.author.username
     class Meta:
