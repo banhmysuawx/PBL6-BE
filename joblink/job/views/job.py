1b48629b484f9b6e2afca2095f877904ba8a451f
Tri An Nguyen <triandn43@gmail.com>
2022-11-11 16:23:28 +0700

    sum_job

--- joblink/job/views/job.py
+++ joblink/job/views/job.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
 from job.models.job import Job
 from job.serializers.job import JobSerializer
-
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated,IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 class JobView(generics.ListCreateAPIView):
     serializer_class = JobSerializer
     queryset = Job.objects.filter(is_active=True)
@@ -9,3 +11,9 @@ class JobView(generics.ListCreateAPIView):
 class JobDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = JobSerializer
     queryset = Job.objects.filter(is_active=True)
+
+class SumJobView(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    def get(self, request , format =None):
+        sum_job = Job.objects.all().count()
+        return Response({'count': sum_job})
