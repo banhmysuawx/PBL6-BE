7d0579fe9030940abd43443a846cbfec896aeaac
Ngo Nguyen Hoang Dung <nnhoangdungdh@gmail.com>
2022-11-16 15:42:08 +0700

    feature/implement get category contains job in each company

--- joblink/job/views/job_category.py
+++ joblink/job/views/job_category.py
@@ -1,6 +1,12 @@
 from rest_framework import generics,mixins
+from rest_framework import viewsets,status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from job.serializers.job_category import JobCategorySerializer
 from job.models.job_category import JobCategory
+from job.services.job_category_service import JobCategoryService
+from job.serializers.job_category import getJobCategorySerializer
+
 
 class JobCategoryView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
     serializer_class = JobCategorySerializer
@@ -27,3 +33,20 @@ class JobCategoryDetailView(generics.GenericAPIView, mixins.RetrieveModelMixin, 
         return self.destroy(request,*args, **kwargs )
 
 
+class CategoryJobInCompany(viewsets.ViewSet):
+    serializer_class = JobCategorySerializer
+    queryset = JobCategory.objects.all()
+
+    @action(methods=['GET'],detail=False)
+    def get_category_by_company(self, request, *args, **kwargs):
+        id_company = self.request.query_params.get("company_id", None)
+        if id_company != None :
+            data = JobCategoryService.getJob(id_company)
+            data = getJobCategorySerializer(data,many=True).data
+            return Response(data=data,status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+    
