618f75f95a72a0a46e5e5f0efe2c8a508f5e7516
triandn <an.tri.nguyen@mgm-tp.com>
2022-11-24 22:58:39 +0700

    modify companies

--- joblink/companies/urls.py
+++ joblink/companies/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('companies', CompanyListView.as_view()),
     path('companies/<int:pk>', CompanyDetailView.as_view()),
     path('companies/top_company', TopCompanyListView.as_view()),
-    path('companies/sum_company', CountCompanyView.as_view())
+    path('companies/sum_company', CountCompanyView.as_view()),
+    path('companies-list/admin', CompanyListAdminView.as_view()),
+    path('companies-detail/admin', CompanyDetailAdminView.as_view())
 ]
