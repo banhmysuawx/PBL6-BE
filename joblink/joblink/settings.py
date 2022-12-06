37671614ca37edd6b4bdc443c6c54233d5db2c32
Dinh Truong Quang <dinhtq@nal.vn>
2022-12-07 00:12:07 +0700

    add ELK

--- joblink/joblink/settings.py
+++ joblink/joblink/settings.py
@@ -60,7 +60,9 @@ INSTALLED_APPS = [
     "applicants",
     "seekers",
     "favorites",
-    "statisticals"
+    "statisticals",
+    # "django_elasticsearch_dsl",
+    # "django_elasticsearch_dsl_drf",
 ]
 
 MIDDLEWARE = [
@@ -233,3 +235,52 @@ sentry_sdk.init(
     traces_sample_rate=1.0,
     send_default_pii=True,
 )
+
+# Elasticsearch
+# ELASTICSEARCH_DSL={
+#     'default': {
+#         'hosts': 'localhost:9200'
+#     },
+# }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'logstash': {
+            'level': 'WARNING',
+            'class': 'logstash.TCPLogstashHandler',
+            'host': 'localhost',
+            'port': 50000,  # Default value: 5000
+            'version': 1,
+            'message_type': 'django_logstash',  # 'type' field in logstash message. Default value: 'logstash'.
+            'fqdn': False,  # Fully qualified domain name. Default value: false.
+            'tags': ['django.request'],  # list of tags. Default: None.
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['logstash'],
+            'level': 'WARNING',
+            'propagate': True,
+        },
+    }
+}
