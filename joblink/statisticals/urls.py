4ff6d498b24a3f6720af53eb5f54afe5df1ea4bf
An Tri Nguyen <an.tri.nguyen@mgm-tp.com>
2022-12-25 08:38:05 +0700

    chart seeker and employer

--- joblink/statisticals/urls.py
+++ joblink/statisticals/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import TopCompany,TopJobCategory,TopJobHighSalary
+from .views import *
 urlpatterns = [
     path('statisticals/top-company', TopCompany.as_view()),
     path('statisticals/top-job-category', TopJobCategory.as_view()),
-    path('statisticals/top-job-high-salary', TopJobHighSalary.as_view())
+    path('statisticals/top-job-high-salary', TopJobHighSalary.as_view()),
+    path('statisticals/seeker-employer-by-month', SeekerAndEmployerByMonth.as_view())
 ]
--- joblink/statisticals/views.py
+++ joblink/statisticals/views.py
@@ -4,12 +4,18 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
+from django.db.models import Count
 from rest_framework import generics
 from companies.models import Company
 from job.models import Job
 from job.models import JobCategory
 from collections import defaultdict
 from accounts.models import User
+from django.db.models import Func
+from django.db.models.functions import TruncMonth
+from django.http import JsonResponse
+import json
+from django.http import HttpResponse
 # Create your views here.
 class TopCompany(APIView):
 
@@ -82,12 +88,78 @@ class TopJobHighSalary(APIView):
         }
         return Response(data, status=status.HTTP_200_OK)
 
-# class SeekerAndEmployerByMonth(APIView):
-#     def get(self, request, format =None):
-#         labels = [] 
-#         chartLabels = "Listing Performance"
-#         seekerByMonth = []
-#         employerByMonth = []
-#         for user in User.objects.filter():
-            
-#         return Response("Ok")
+def Extract(field, date_field='DOW'):
+    template = "EXTRACT({} FROM %(expressions)s::timestamp)".format(date_field)
+    return Func(field, template=template)
+
+class SeekerAndEmployerByMonth(APIView):
+    def get(self, request, format =None):
+        labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'] 
+        seekerList = list(User.objects.filter(role='seeker').annotate(month=TruncMonth('created_at')).values('month').annotate(count_id=Count('id')).order_by('-month')[:12])
+        employerList = list(User.objects.filter(role='employer').annotate(month=TruncMonth('created_at')).values('month').annotate(count_id=Count('id')).order_by('-month')[:12])
+        seeker_count = []
+        seeker_count = [0 for i in range(12)] 
+        employer_count = []
+        employer_count = [0 for i in range(12)]
+        for user in seekerList:
+            if  user['month'].month == 1:
+                seeker_count[0]=user['count_id']
+            elif user['month'].month == 2:
+                seeker_count[1]=user['count_id']
+            elif user['month'].month == 3:
+                seeker_count[2]=user['count_id']
+            elif user['month'].month == 4:
+                seeker_count[3]=user['count_id']
+            elif user['month'].month == 5:
+                seeker_count[4]=user['count_id']
+            elif user['month'].month == 6:
+                seeker_count[5]=user['count_id']
+            elif user['month'].month == 7:
+                seeker_count[6]=user['count_id']
+            elif user['month'].month == 8:
+                seeker_count[7]=user['count_id']
+            elif user['month'].month == 9:
+                seeker_count[8]=user['count_id']
+            elif user['month'].month == 10:
+                seeker_count[9]=user['count_id']
+            elif user['month'].month == 11:
+                seeker_count[10]=user['count_id']
+            elif user['month'].month == 12:
+                seeker_count[11]=user['count_id']
+        for user in employerList:
+            if  user['month'].month == 1:
+                employer_count[0]=user['count_id']
+            elif user['month'].month == 2:
+                employer_count[1]=user['count_id']
+            elif user['month'].month == 3:
+                employer_count[2]=user['count_id']
+            elif user['month'].month == 4:
+                employer_count[3]=user['count_id']
+            elif user['month'].month == 5:
+                employer_count[4]=user['count_id']
+            elif user['month'].month == 6:
+                employer_count[5]=user['count_id']
+            elif user['month'].month == 7:
+                employer_count[6]=user['count_id']
+            elif user['month'].month == 8:
+                employer_count[7]=user['count_id']
+            elif user['month'].month == 9:
+                employer_count[8]=user['count_id']
+            elif user['month'].month == 10:
+                employer_count[9]=user['count_id']
+            elif user['month'].month == 11:
+                employer_count[10]=user['count_id']
+            elif user['month'].month == 12:
+                employer_count[11]=user['count_id']
+
+        data = {
+            "labels":labels,
+            "seeker":seeker_count,
+            "employer":employer_count
+        }
+
+        return Response(data, status=status.HTTP_200_OK)
+
+class RatingCompany(APIView):
+    def get(self, request, format = None):
+        return Response("Ok")
