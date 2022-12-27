e45403de1ba4ab7de1f34b8b2b938f9559ec7942
An Tri Nguyen <an.tri.nguyen@mgm-tp.com>
2022-12-27 11:10:12 +0700

    add api get company profile

--- joblink/companies/views.py
+++ joblink/companies/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from rest_framework.permissions import AllowAny, IsAuthenticated,IsAuthenticatedOrReadOnly
 from rest_framework import status
 from companies.models import Company
-from companies.serializers import CompanySerializer
+from companies.serializers import CompanySerializer,CompanyProfileSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework import generics
-
+from django.http import HttpResponse
+from reviews.models import Review
+import json
 # API GET and POST requests
 class CompanyListView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticatedOrReadOnly,)
@@ -53,3 +55,14 @@ class CompanyListAdminView(generics.ListCreateAPIView):
 class CompanyDetailAdminView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class= CompanySerializer
     queryset = Company.objects.all()
+
+class CompanyProfileView(APIView):
+    def get(self, request, *args, **kwargs):
+        user_id = self.kwargs.get('user_id')
+        company_profile = []
+        dict_company = {"company": []}
+        for company in Company.objects.all():
+            if company.user.id == user_id:
+                company_profile.append(company)
+        serializer = CompanyProfileSerializer(company_profile, many = True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
